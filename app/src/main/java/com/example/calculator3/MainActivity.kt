57e9290d8545c2c480a2b4a7b3f17ebeb2e46b2d
MichaelSenla <mikhail_chanov@senla.eu>
2021-11-09 11:39:04 +0300

    Beginning

---
+++ app/src/main/java/com/example/calculator3/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.calculator3
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
